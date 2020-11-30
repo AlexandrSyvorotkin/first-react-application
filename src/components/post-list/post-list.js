df1c42d7729b0cc7591dc7dd37ba00c49e57d806
Alex <Aleks031994@mail.ru>
2020-11-30 22:44:04 +0300

    end application + refactoring and modifying  react components

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,30 +1,27 @@
 import React from 'react';
 
 import PostListItem from '../post-list-item/post-list-item';
-import { ListGroup } from 'reactstrap';
-
 import './post-list.css';
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
-    const elements = posts.map((it) => {
-        const {id, ...itProps} = it;
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
         return (
-
             <li key={id} className='list-group-item'>
-                <PostListItem {...itProps} 
-                    onDelete={()=> onDelete(id)}/>
-
+                <PostListItem 
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+                onToggleImportant={() => onToggleImportant(id)}
+                onToggleLiked={()=> onToggleLiked(id)}/>
             </li>
         )
-    })
+    });
 
     return (
-
-        <ListGroup className="app-list">
+        <ul className="app-list list-group">
             {elements}
-        </ListGroup>
-
+        </ul>
     )
 }
 
